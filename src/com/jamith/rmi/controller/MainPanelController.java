c21356837bfe8d62976b0d1e2266ac8182ed8150
Jamith Nimantha <jamithnimantha@gmail.com>
2020-11-18 22:27:28 +0530

    Implemented logout functionality

--- src/com/jamith/rmi/controller/MainPanelController.java
+++ src/com/jamith/rmi/controller/MainPanelController.java
@@ -1,19 +1,27 @@
 package com.jamith.rmi.controller;
 
+import com.jamith.rmi.service.ServiceFactory;
+import com.jamith.rmi.service.ServiceHandler;
+import com.jamith.rmi.service.UserService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author Jamith Nimantha
  */
-public class MainPanelController {
+public class MainPanelController implements Initializable {
 
     @FXML
     private Button btnStartSurvey;
@@ -23,6 +31,8 @@ public class MainPanelController {
 
     @FXML
     private Button btnLogout;
+
+    private UserService userService;
 
     @FXML
     void btnAnalyticalOnAction(ActionEvent event) throws IOException {
@@ -38,14 +48,24 @@ public class MainPanelController {
 
     @FXML
     void btnLogoutOnAction(ActionEvent event) throws IOException {
-        Stage stage = (Stage) btnLogout.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/com/jamith/rmi/view/Login.fxml"));
-        Parent parent = fxmlLoader.load();
-        Scene scene = new Scene(parent);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.centerOnScreen();
-        stage.show();
+        boolean sample = userService.logout("sample");
+        if (sample) {
+            Stage stage = (Stage) btnLogout.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/com/jamith/rmi/view/Login.fxml"));
+            Parent parent = fxmlLoader.load();
+            Scene scene = new Scene(parent);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.centerOnScreen();
+            stage.show();
+
+            Alert a = new Alert(Alert.AlertType.INFORMATION, "Logout Successfully", ButtonType.OK);
+            a.initOwner(stage);
+            a.setTitle("Logout Successfully !");
+            a.setHeaderText(null);
+            a.setContentText("You have Logout Successfully!");
+            a.show();
+        }
     }
 
     @FXML
@@ -58,5 +78,22 @@ public class MainPanelController {
         stage.setResizable(false);
         stage.centerOnScreen();
         stage.show();
+    }
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            userService = (UserService) ServiceHandler.getInstance().getService(ServiceFactory.ServiceType.USER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
